54104f5ac86f9b02d877530bda3e276c4e2ca5ce
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-05-08 15:55:41 +0500

    homepage done

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,29 @@ export interface ChildProps {
 	children: ReactNode
 }
 
-export interface LngParams{
-	params:{lng:string}
+export interface LngParams {
+	params: { lng: string }
+}
+
+export interface ICourse {
+	title: string
+	previewImage: string
+	level: string
+	author: {
+		image: string
+		name: string
+	}
+	oldPrice: number
+	currentPrice: number
+}
+
+export interface ICategory {
+	label: string
+	icon: string
+}
+
+export interface IInstructor {
+	name: string
+	image: string
+	job: string
 }
